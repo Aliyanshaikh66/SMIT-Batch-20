55663443b2904cc48678401ecf5786395320c21a
Aliyan Shaikh <aliyanzahid02@gmail.com>
2026-01-23 09:41:21 +0500

    code

--- 3. javaScript/alert/main.js
+++ 3. javaScript/alert/main.js
@@ -10,26 +10,50 @@
 // var isStudent = true; //boolean
 
 //Method 1
-alert("Karachi"); // alert as a string
+// alert("Karachi"); // alert as a string
 
 //Method 2
-var cities = "Karachi "; //string
-alert(cities); // alert as a variable
+// var cities = "Karachi "; //string
+// alert(cities); // alert as a variable
 
 //Method 3
-var cities = "Karachi "; //string
-alert("The City of Light is " + cities); // alert as a variable + String 
+// var cities = "Karachi "; //string
+// alert("The City of Light is " + cities); // alert as a variable + String 
 
 //Method 4
 // Two variables addition and show them in alert box
-var cities = "Karachi "; //string
-var discription = "The City of Light";//string
-alert(cities +  discription );
+// var cities = "Karachi "; //string
+// var discription = "The City of Light";//string
+// alert(cities +  discription );
+
+// there are 3 types of variable
+// 1 strting ("" or '' )
+// 2 number ("12345")
+// 3 boolean (true or false)
+
+// alert(My Name is Aliyan); // simple tex
+// alert('My Name is Aliyan'); // text in single quotes String
+// alert("My Name is Aliyan"); // text in double quotes String
+// alert("12345"); // number
+
+// alert("12345"); // number
+
+// var user = "Aliyan";
+// alert("Yor are Verifed " + user);
+// alert("Welcome " + user + " to our website!");
 
 
+// var subject = 'JavaScript';
+// alert(subject);
+// alert("Hello " + user +  age);
 
+// var user = prompt("Enter your name");
+// console.log(user);
 
+// var firstname = "Aliyan";
+// var num = 12345678;
+// alert(firstname + " " + num);
 
-
-
-
+var user = prompt("Enter your name");
+var rollno = prompt("Enter your roll number");
+alert("welcome " + user + " Your roll Number is " + rollno);
--- 3. javaScript/variable/main.js
+++ 3. javaScript/variable/main.js
@@ -5,19 +5,64 @@
 // var isStudent = true; //boolean
 
 //Method 1
-alert("Karachi"); // alert as a string
+// alert("Karachi"); // alert as a string
 
-//Method 2
-var cities = "Karachi "; //string
-alert(cities); // alert as a variable
+// //Method 2
+// var cities = "Karachi "; //string
+// alert(cities); // alert as a variable
 
-//Method 3
-var cities = "Karachi "; //string
-alert("The City of Light is " + cities); // alert as a variable + String 
+// //Method 3
+// var cities = "Karachi "; //string
+// alert("The City of Light is " + cities); // alert as a variable + String 
 
-//Method 4
-// Two variables addition and show them in alert box
-var cities = "Karachi "; //string
-var discription = "The City of Light";//string
-alert(cities +  discription );
+// //Method 4
+// // Two variables addition and show them in alert box
+// var cities = "Karachi "; //string
+// var discription = "The City of Light";//string
+// alert(cities +  discription );
+
+//variable for Number
+
+// var firstname = "Aliyan";
+// var num = 12345678;
+// alert(firstname + " " + num);
+
+// var user = prompt("Enter your name");
+// var rollno = prompt("Enter your roll number");
+// alert("welcome " + user + " Your roll Number is " + rollno);
+
+var firstname = "Hello";
+var lastname = "Aliyan";
+var user = (firstname + " " + lastname);
+alert(user);
+
+
+//Basic Programming Concepts / Symbols:
+//
+// brakets paranthisis ()
+// curly brakets {}
+// square brakets []
+// semicolon ;
+// colon :
+// comma ,
+// dot .
+// quotes '' ""
+// equal sign =
+// is equal to sign ==
+// is not equal to sign !=
+// underscoure _
+// plus sign +
+// minus sign -
+// multiplication sign *
+// percent sign %
+// division sign /
+// forwad slash //
+// back slash \
+// exclamation mark ! 
+// greater than sign >
+// less than sign <
+// greater than or equal to sign >=
+// less than or equal to sign <=
+// vertical bar |
+
 
